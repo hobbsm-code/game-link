4def2c46aa01ac9537265d9a7595cd49a2b37a69
Edward Ofori <tetraed.llc@gmail.com>
2025-02-08 17:26:42 -0500

    Added code to define the schema for Graphql Game.tx

--- Develop/server/src/models/Book.ts
+++ Develop/server/src/models/Book.ts
@@ -1,34 +1,40 @@
 import { Schema, type Document } from 'mongoose';
 
-export interface BookDocument extends Document {
-  bookId: string;
+export interface GameDocument extends Document {
+  gameId: string;
   title: string;
-  authors: string[];
-  description: string;
-  image: string;
-  link: string;
+  short_description: string;
+  game_url: string;
+  genre: string;
+  platform: string;
+  publisher: string;
+  developer: string;
+  release_date: string;
+  freetogame_profile_url: string;
+  id: number;
+  thumbnail: string;
 }
 
 // This is a subdocument schema, it won't become its own model but we'll use it as the schema for the User's `savedBooks` array in User.js
-const bookSchema = new Schema<BookDocument>({
-  authors: [
+const gameSchema = new Schema<GameDocument>({
+  publisher: [
     {
       type: String,
     },
   ],
-  description: {
+  short_description: {
     type: String,
     required: true,
   },
   // saved book id from GoogleBooks
-  bookId: {
+  gameId: {
     type: String,
     required: true,
   },
-  image: {
+  freetogame_profile_url: {
     type: String,
   },
-  link: {
+  game_url: {
     type: String,
   },
   title: {
@@ -37,4 +43,4 @@ const bookSchema = new Schema<BookDocument>({
   },
 });
 
-export default bookSchema;
+export default gameSchema;
