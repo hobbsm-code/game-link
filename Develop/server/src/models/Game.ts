cb842919c917f52a98b84f516641332a86df82ad
Mark Hobbs <hobbsm121@gmail.com>
2025-02-09 12:10:52 -0500

    Created schema for User and Game

---
+++ Develop/server/src/models/Game.ts
@@ -0,0 +1,50 @@
+import { Schema, type Document } from 'mongoose';
+
+export interface GameDocument extends Document {
+  id: string;
+  title: string;
+  short_description: string;
+  thumbnail: string;
+  genre: string;
+  game_url: string;
+  platform: string;
+  time_played: number;
+}
+
+const gameSchema = new Schema<GameDocument>({
+  id: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  short_description: {
+    type: String,
+    required: true,
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+  },
+  genre: {
+    type: String,
+    required: true,
+  },
+  game_url: {
+    type: String,
+    required: true,
+  },
+  platform: {
+    type: String,
+    required: true,
+  },
+  time_played: {
+    type: Number,
+    default: 0,
+    required: false,
+  },
+});
+
+export default gameSchema;
