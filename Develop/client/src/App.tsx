9ac340fd2c03e41a25c8fc142355854930996fe6
Edward Ofori <tetraed.llc@gmail.com>
2025-02-12 02:43:01 -0500

    Added code for leaderboard6

--- Develop/client/src/App.tsx
+++ Develop/client/src/App.tsx
@@ -11,8 +11,8 @@ import Navbar from './components/Navbar';
 
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3001/graphql',
-  // uri: '/graphql',
+  // uri: 'http://localhost:3001/graphql',
+  uri: 'https://game-link-backend.onrender.com/graphql',
 });
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
